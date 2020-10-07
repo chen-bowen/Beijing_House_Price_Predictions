ffa03e882c5e21a4f98e9586303940e25db9f335
Bowen Chen <chenbowen184@gmail.com>
2020-10-07 10:21:28 -0700

    update run.sh to make the port dynamic

--- model_api/run.sh
+++ model_api/run.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 export IS_DEBUG=${DEBUG:-false}
+if [[ -z "${PORT}" ]]; then
+    PORT=5000
+else
+    PORT="${PORT}"
+fi
+
 exec gunicorn --bind 0.0.0.0:$PORT --access-logfile - --error-logfile - run:application
