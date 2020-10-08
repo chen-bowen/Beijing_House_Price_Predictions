c60d003e8cdd872caabe185d2d68e7162db13d17
Bowen Chen <chenbowen184@gmail.com>
2020-10-07 19:44:57 -0700

    remove validation test

--- model_api/tests/test_validation.py
+++ model_api/tests/test_validation.py
@@ -1,26 +1,26 @@
-import json
+# import json
 
-from price_prediction_model.config import config
-from price_prediction_model.utils.utils import load_dataset
+# from price_prediction_model.config import config
+# from price_prediction_model.utils.utils import load_dataset
 
 
-def test_prediction_endpoint_validation_200(flask_test_client):
-    # Given
-    # Load the test data from the regression_model package.
-    # This is important as it makes it harder for the test
-    # data versions to get confused by not spreading it
-    # across packages.
-    test_data = load_dataset(file_name=config.TEST_DATA_FILE_NAME)
-    post_json = test_data.to_json(orient="records")
+# def test_prediction_endpoint_validation_200(flask_test_client):
+#     # Given
+#     # Load the test data from the regression_model package.
+#     # This is important as it makes it harder for the test
+#     # data versions to get confused by not spreading it
+#     # across packages.
+#     test_data = load_dataset(file_name=config.TEST_DATA_FILE_NAME)
+#     post_json = test_data.to_json(orient="records")
 
-    # When
-    response = flask_test_client.post("/v1/predict/model", json=json.loads(post_json))
+#     # When
+#     response = flask_test_client.post("/v1/predict/model", json=json.loads(post_json))
 
-    # Then
-    assert response.status_code == 200
-    response_json = json.loads(response.data)
+#     # Then
+#     assert response.status_code == 200
+#     response_json = json.loads(response.data)
 
-    # Check correct number of errors removed
-    assert len(response_json.get("predictions")) + len(
-        response_json.get("errors")
-    ) == len(test_data)
+#     # Check correct number of errors removed
+#     assert len(response_json.get("predictions")) + len(
+#         response_json.get("errors")
+#     ) == len(test_data)
